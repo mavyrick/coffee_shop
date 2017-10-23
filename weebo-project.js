45e19b9ac065a6b7778628244a710a0576046bdf
Josh Sorokin <josh@wee.bo>
2017-10-23 23:19:51 +0300

    non-linear price-range slider working

--- weebo-project.js
+++ weebo-project.js
@@ -1,126 +1,128 @@
-
-
-// import {coffeeData} from 'coffee-data.js'
-// var coffeeData = coffeeData();
-
-
-// const proxyurl = "https://cors-anywhere.herokuapp.com/";
-// // const proxyurl = "https://crossorigin.me/"
-// const url = " http://uk.gomerchants.net/inc/twenga.php?keyword=%7Bcoffee%7D&method=searchOffer"; // site that doesn’t send Access-Control-*
-// fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-// .then(response => response.text())
-// .then(contents => console.log(contents))
-// .catch(console.log("Can’t access " + url + " response. Blocked by browser?"))
-
-// $.getScript("coffee-data.js", function() {
-
-//  alert("Script loaded but not necessarily executed.");
-
-// });
-
 var app = angular.module('app', [])
 
+// Filter for removing duplicate results when using ng-repeat.
+
 app.filter('unique', function() {
-   // we will return a function which will take in a collection
-   // and a keyname
-   return function(collection, keyname) {
-      // we define our output and keys array;
-      var output = [], 
-      keys = [];
-      
-      // we utilize angular's foreach function
-      // this takes in our original collection and an iterator function
-      angular.forEach(collection, function(item) {
-          // we check to see whether our object exists
-          var key = item[keyname];
-          // if it's not already part of our keys array
-          if(keys.indexOf(key) === -1) {
-              // add it to our keys array
-              keys.push(key); 
-              // push this item to our final output array
-              output.push(item);
-            }
-          });
-      // return our array which should be devoid of
-      // any duplicates
-      return output;
-    };
+
+ return function(collection, keyname) {
+
+  var output = [], 
+  keys = [];
+
+  angular.forEach(collection, function(item) {
+
+    var key = item[keyname];
+
+    if(keys.indexOf(key) === -1) {
+
+      keys.push(key); 
+
+      output.push(item);
+    }
   });
 
-
-
-// $(document).ready(function() {
-
-  // $("#slider-range").change(function() {
-
-
-
- // $scope.priceFunction = function($scope.sliderValues) {
- //    if ($scope.sliderValues < 100) {
- //      return (product.attrs.att_old == 0)
- //    } else if (ageValue == 1) {
- //      return (product.attrs.att_old == 1)
- //    } else {
- //      return ""
- //    }
- //  }
-
-// })
-
-// })
-
-  // $scope.sliderValues = $scope.$on('$viewContentLoaded', function() {
-  //   $( "#slider-range" ).slider( "values");
-  // })
-
-  app.controller('CoffeeController', function($scope, $http) {
-
-//   $http.get('coffee-data.json')
-//        .then(function(res){
-//           $scope.coffeeData = coffeeD;                
-//         });
-// });
-
-// $scope.keyword = function(coffeeItem) {
-//   return document.getElementById("product-search-input").value
-// }
-
-// $scope.keyword = "ipad"
-
-// document.getElementById("product-search-input").value
-
-
-$scope.productSearch = function () {
-  $scope.keyword = document.getElementById("product-search-input").value;
-// $scope.keyword = ""
-$http({
-  method: "GET",
-  url: "http://uk.gomerchants.net/inc/demo.php?keyword=" + $scope.keyword,
-}).then(function mySuccess(response) {
-  $scope.coffeeData = response.data;
-}, function myError(response) {
-  $scope.coffeeData = response.statusText;
+  return output;
+};
 });
-}
 
+// Main controller
 
-// $scope.coffeeData = coffeeJSON.data;
+app.controller('CoffeeController', function($scope, $http) {
 
-$scope.sortType = "";
+  $scope.coffeeData = [];
+
+  $scope.productSearch = function () {
+    $scope.keyword = document.getElementById("product-search-input").value;
+
+    $http({
+      method: "GET",
+      url: "http://uk.gomerchants.net/inc/demo.php?keyword=" + $scope.keyword,
+    }).then(function mySuccess(response) {
+      $scope.coffeeData = response.data;
+    }, function myError(response) {
+      $scope.coffeeData = response.statusText;
+    });
+  }
+
+  // For selecting how something is sorted.
+
+  $scope.sortType = "";
+
+// For selecting how something is filtered.
 
 $scope.filterType = ""
+
+// For searching through search results retrieved from main product search.
 
 $scope.searchFilter = function(coffeeItem) {
   return (coffeeItem.description.includes(document.getElementById("filter-search-input").value) || coffeeItem.name.includes(document.getElementById("filter-search-input").value))
 };
 
-// $("#slider-range").on("slidechange", function(event, ui) {
-  angular.element(document).ready(function() {
-   $scope.priceFilter = function(coffeeItem) {
-    var priceValues = $("#slider-range" ).slider("values");
-    return (coffeeItem.price_raw >= priceValues[0]) && (coffeeItem.price_raw <= priceValues[1]);
+// Price range slider with non-linear values.
+// This slider allow the user to choose a price range.
+// The slider works in a non-linear fashion!!!
+// Lower numbers will slide slower and higher numbers will slide faster.
+// This makes it much easier to choose a price range as lower prices are more likely to be selected and usually more particular.
+// Slider also makes far right slide position maximum price.
+// -Josh
+
+$scope.priceFilter = ""
+angular.element(document).ready(function() {
+  $("#slider").on( "slidechange", function( event, ui ) {
+    $scope.$apply(function() {
+     $scope.priceFilter = function(coffeeItem) {
+      var min = $("#slider").slider("values")[0];
+      var max = $("#slider").slider("values")[1];
+        // Make values non-linear
+        var nonLinearPriceMin = Math.easeIn(min, 0, 2001, 2.5);
+        var nonLinearPriceMax = Math.easeIn(max, 1, 2001, 2.5);
+        if (nonLinearPriceMax == 2001) {
+          return (coffeeItem.price_raw >= nonLinearPriceMin);
+        } else {
+          return (coffeeItem.price_raw >= nonLinearPriceMin) && (coffeeItem.price_raw <= nonLinearPriceMax);
+        }
+      };
+    });
+  });
+});
+
+  // Function for making values non-linear
+
+  Math.easeIn = function (val, min, max, strength) {
+    val /= max;
+    return (max-1)*Math.pow(val, strength) + min;
   };
-})
+
+
+  $( function() {
+    $( "#slider" ).slider({
+      change: function( event, ui) { var scope = angular.element($("#slider")).scope();
+      scope.$apply(function() {
+        scope.filterType = scope.priceFilter
+      })
+    },
+    range: true,
+    min: 0,
+    max: 2001,
+    values: [0, 2001],
+    slide: function( event, ui ) {
+
+      var nonLinearMin = Math.easeIn(ui.values[0], 0, 2001, 2.5);
+      var nonLinearMax = Math.easeIn(ui.values[1], 0, 2001, 2.5);
+
+      $('#min-amount').val("$" + Math.round(nonLinearMin));      
+      if (ui.values[1] == 2001) {
+        $("#max-amount").val("max")
+      } else {
+        $("#max-amount").val("$" + Math.round(nonLinearMax))
+      }
+    }
+  });
+    $( "#min-amount" ).val( "$" + $( "#slider" ).slider( "values", 0 ));
+
+    $("#max-amount").val("max");
+
+  });
 
   $scope.categoryFilter = function(coffeeItem) {
     var e = document.getElementById("category-dropdown-select")
@@ -134,10 +136,9 @@ $scope.searchFilter = function(coffeeItem) {
         text: 'Item 2'
       }];   
     }
-
-//   var e = document.getElementById("ddlViewBy");
-// var strUser = e.options[e.selectedIndex].value;
 };
+
+// Various data filters
 
 $scope.brandFilter = function(coffeeItem) {
   return (document.getElementById("brand-option").value)
@@ -146,17 +147,8 @@ $scope.brandFilter = function(coffeeItem) {
 $scope.merchantFilter = function(coffeeItem) {
   return (document.getElementById("merchant-option").value)
 };
-// })
 
-$scope.maxPrice = "test"
-
-
-// $( "#slider-range" ).on( "slidechange", function( event, ui ) {
-
-//   $scope.sliderValues = 
-
-//   $( "#slider-range" ).slider( "values");
-// })
+// For highlighting names on hover
 
 $scope.nameHighlightEnter = function() {
   $(".name-link-" + $scope.coffeeData[this.$index].id).css("color", "#f03d41");
@@ -165,6 +157,8 @@ $scope.nameHighlightEnter = function() {
 $scope.nameHighlightLeave = function() {
   $(".name-link-" + $scope.coffeeData[this.$index].id).css("color", "black");
 }
+
+// For only showings a certain number of items until user scrolls to bottom of the page
 
 $scope.itemLimit = 20;
 angular.element(document).ready(function() {
@@ -176,8 +170,9 @@ angular.element(document).ready(function() {
    })
   },100)
 })
+})
 
-});
+// Events triggered as user scrolls down
 
 window.onscroll = function() {
   var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
@@ -186,13 +181,19 @@ window.onscroll = function() {
   var headerTop = document.getElementById("coffee-header-top")
   var headerBottom = document.getElementById("coffee-header-bottom")
   var filterDisplay = document.getElementById("filter-display")
-  var headerSlider = document.getElementById("header-price-slider")
+  var priceSlider = document.getElementById("price-slider")
 
   // var testFooter = document.getElementById("test-footer")
   // var scrolledOnce = false;
   if (scrollTop >= minimizeHeader.offsetTop) {
     headerBottom.classList.add("search-fixed")
     minimizeHeader.style.paddingBottom = "50px";
+    // priceSlider.style.position = "fixed";
+    priceSlider.style.top = "7.5px";
+
+    // priceSlider.classList.add("slider-minimized")
+    // priceSlider.classList.remove("slider-maximized")
+
     // coffeeHeader.style.visibility = "hidden"
 
     // cartHeader.style.visibility = "visible"
@@ -200,105 +201,21 @@ window.onscroll = function() {
   } else {
     headerBottom.classList.remove("search-fixed")
     headerTop.style.paddingBottom = "0px";
-    minimizeHeader.style.paddingBottom = "0px"
+    minimizeHeader.style.paddingBottom = "0px";
+
+    priceSlider.style.top = "109px"
+    priceSlider.style.left = "88px"
+    // priceSlider.classList.add("slidier-maximized")
+    // priceSlider.classList.remove("slider-minimized")
+    // priceSlider.style.position = "absolute";
+
+
         // coffeeHeader.style.visibility = "visible"
     // $("#cart-header").slideUp(400), function() { 
       // cartHeader.style.visibility = "hidden"
     }
 
-    if (scrollTop >= filterDisplay.offsetTop) {
-      headerSlider.style.display = "block";
-    }
+    // if (scrollTop >= filterDisplay.offsetTop) {
+    //   // headerSlider.style.display = "block";
+    // }
   }
-
-//   $http({
-//     method: "POST",
-//     url: "/http://uk.gomerchants.net/inc/twenga.php",
-//     params: {
-//      'keyword': 'coffee',
-//      'method': 'searchOffer'
-//    }
-//  }).then(function mySuccess(response) {
-//   $scope.myWelcome = response.data;
-// }, function myError(response) {
-//   $scope.myWelcome = response.statusText;
-// });
-// });
-
-// //cors-anywhere.herokuapp.comhttps:/http://uk.gomerchants.net/inc/twenga.php?keyword=coffee&method=searchOffer
-
-// $scope.donePopUp = function(title) {
-//     if (title.includes("DONE") == true) {
-//       var popUp = document.getElementById("done-popup-" + $scope.products[this.$index].id);
-//       popUp.style.visibility = 'visible'
-//     }
-//   }
-
-
-
-
-// mainApp.service('UserService', function($http, $q){
-
-//    this.getUsers = function(page = 1, limit = 100, sort = 'id', direction = 'desc') {
-
-//         var dfrd = $q.defer();
-//         $http.get('https://www.your-website.com/api/users.json', 
-//             {
-//                 params:{page: page, limit: limit, sort: sort, direction: direction},
-//                 headers: {Authorization: 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='}
-//             }
-//         )
-//         .then(function(response) {
-//             if ( response.data.success == true ) { 
-
-//             } else {
-
-//             }
-//         }, function(x) {
-
-//             dfrd.reject(true);
-//         });
-//         return dfrd.promise;
-//    }
-
-// });
-
-// angular.module('ui.filters').filter('unique', function () {
-
-//   return function (items, filterOn) {
-
-//     if (filterOn === false) {
-//       return items;
-//     }
-
-//     if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
-//       var hashCheck = {}, newItems = [];
-
-//       var extractValueToCompare = function (item) {
-//         if (angular.isObject(item) && angular.isString(filterOn)) {
-//           return item[filterOn];
-//         } else {
-//           return item;
-//         }
-//       };
-
-//       angular.forEach(items, function (item) {
-//         var valueToCheck, isDuplicate = false;
-
-//         for (var i = 0; i < newItems.length; i++) {
-//           if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
-//             isDuplicate = true;
-//             break;
-//           }
-//         }
-//         if (!isDuplicate) {
-//           newItems.push(item);
-//         }
-
-//       });
-//       items = newItems;
-//     }
-//     return items;
-//   };
-// });
-
